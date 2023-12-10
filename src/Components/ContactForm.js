2d7ce62e087f3848201ac467219a1fcb21aec0c6
Ossama <otenessi@gmail.com>
2023-12-10 16:26:35 -0500

    feat(contact form): add pop up

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,52 +1,85 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AnimatePresence, motion } from 'framer-motion';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import emailjs from 'emailjs-com';
-
 
 emailjs.init('PjQa-I7WHUNVQp8jU'); 
 
 const ContactForm = ({ isOpen, onClose }) => {
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+
+
     if (!isOpen) return null;
-  
+
+
     const sendEmail = (event) => {
       event.preventDefault();
-      console.log("hello world");
-  
+      setErrorMessage(''); // Reset the error message on new submission attempt
+
+      const userEmail = event.target.user_email.value;
+      const message = event.target.message.value;
+
+      if (!userEmail.trim() || !message.trim()) {
+        setErrorMessage('Email and message are required.');
+        return;
+      }
+
       emailjs.sendForm("service_mgffp6t", 'template_fbrvsgv', event.target)
         .then((result) => {
           console.log(result.text);
+          setShowSuccessPopup(true); // Show success popup
+          setTimeout(() => {
+            setShowSuccessPopup(false); // Hide popup after some time
+            onClose(); // Close the form
+          }, 1000);
         }, (error) => {
           console.log(error.text);
         });
     };
-  
+
     return (
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <AnimatePresence>
+  {showSuccessPopup && (
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 0.5 }}
+      className="fixed top-5 left-3/5 transform -translate-x-1/2 px-6 py-4 bg-black text-white rounded-lg shadow-lg"
+    >
+      Message sent successfully!
+    </motion.div>
+  )}
+</AnimatePresence>
         <form className="bg-white w-3/4 lg:w-1/2 xl:w-1/3 p-8 rounded-lg shadow-lg flex flex-col items-center" onSubmit={sendEmail}>
           <input name="user_name" className="mb-4 w-full px-3 py-2 border rounded" type="text" placeholder="Your Name" />
           <input name="user_email" className="mb-4 w-full px-3 py-2 border rounded" type="email" placeholder="Your Email" />
           <textarea name="message" className="mb-4 w-full px-3 py-2 border rounded" placeholder="Your Message" rows="3"></textarea>
           
-          <div className="flex-grow"></div> 
+          {errorMessage && <div className="text-red-500 text-sm mb-4">{errorMessage}</div>}
+
+          <div className="flex-grow"></div>
           
           <div className="flex justify-between w-full">
-            <button type="button" onClick={onClose} className="text-left">Close</button>
-            <div className="flex space-x-2">
-              <a href='https://www.linkedin.com/in/ossama-tenessi-00a90a29b/' target='_blank' rel='noopener noreferrer' className="text-lg">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </a>
-              <a href='https://github.com/Le0uss' target='_blank' rel='noopener noreferrer' className="text-lg">
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-            </div>
-          </div>
-  
+  <button type="button" onClick={onClose} className="text-left">Close</button>
+  <div className="flex space-x-2">
+    <a href='https://www.linkedin.com/in/ossama-tenessi-00a90a29b/' target='_blank' rel='noopener noreferrer' className="text-3xl"> {/* Increased size to 2xl */}
+      <FontAwesomeIcon icon={faLinkedin} />
+    </a>
+    <a href='https://github.com/Le0uss' target='_blank' rel='noopener noreferrer' className="text-3xl"> {/* Increased size to 2xl */}
+      <FontAwesomeIcon icon={faGithub} />
+    </a>
+  </div>
+</div>
+
           <button className="bg-gray-500 text-white active:bg-grey-800 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg hover:bg-gray-700 outline-none focus:outline-none mr-1 mb-1 mt-4" type="submit"> Send Message </button>
         </form>
       </div>
     );
-  };
-  
-  export default ContactForm;
-  
-  
+};
+
+export default ContactForm;
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -3,24 +3,25 @@ import { useEffect, useRef, useState, useCallback } from "react";
 import { FaGithub } from 'react-icons/fa';
 import HoverText from "./HoverText"
 import ContactButton from "./ContactButton";
-import img1 from '../images/home.png'
+import img1 from '../images/Bite-Balance.png'
 import img2 from '../images/demo.gif'
 import img3 from '../images/screenshot.png'
-import img4 from '../images/360_F_597374605_tph8vB4RrkKvN2MT1gKeJUKerCR8LYu8.jpg'
+import img4 from '../images/E-commerce.png'
 import img5 from '../images/seat-select.png'
 import img6 from '../images/clicker.gif'
 
 
 const MainPage = () => {
-    const scrollToDescription = (id) => {
-      const element = document.getElementById(`description-${id}`);
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
-    };
-  
-    return (
-      <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
+  const scrollToDescription = (id) => {
+    const element = document.getElementById(`description-${id}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
+  return (
+    <section className="w-full px-8 py-12 max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
         <div>
           <span className="block mb-4 text-xs md:text-sm text-black font-medium">
             Better projects every day
@@ -28,16 +29,18 @@ const MainPage = () => {
           <HoverText className="text-4xl md:text-6xl font-semibold" />
           <p className="text-base md:text-lg text-slate-700 my-4 md:my-6">
             I'm a dedicated web developer always willing to hone my skills in both front-end and back-end development. 
-            My journey in web development is marked by daily commitment to projects, where I'm improving not only my technical 
+            My journey in software development is marked by daily commitment to projects, where I'm improving not only my technical 
             skills but also gaining a deeper understanding of the nuances of web applications.
           </p>
           <ContactButton className="bg-indigo-500 text-white font-medium py-2 px-4 rounded transition-all hover:bg-indigo-600 active:scale-95"/>
         </div>
         <ShuffleGrid scrollToDescription={scrollToDescription} />
-        <DescriptionGrid />
-      </section>
-    );
-  };
+      </div>
+      <DescriptionGrid className="mt-8 md:mt-12" />
+    </section>
+  );
+};
+
 
 const shuffle = (array) => {
   let currentIndex = array.length,
@@ -57,110 +60,111 @@ const shuffle = (array) => {
 };
 
 const squareData = [
-    {
-      id: 1,
-      src: img1,
-      description: (
-        <div className="flex h-full">
-          <img src={img1} alt="Project Description" className="w-1/2 h-auto rounded-lg" />
-          <div className="flex-grow flex flex-col justify-between ml-4">
-            <p>React frontend with server backend integration for web development. Emphasizes dual-terminal use and ease of learning.</p>
-            <div className="text-right">
-              <a href="https://github.com/Le0uss" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
-                <FaGithub /> Check out my Project
-              </a>
-            </div>
+  {
+    id: 1,
+    src: img1,
+    description: (
+      <div className="flex h-full">
+        <img src={img1} alt="BiteBalance" className="w-1/2 h-auto rounded-lg" />
+        <div className="flex-grow flex flex-col justify-between ml-4">
+          <p>Engineered a calorie tracking website in two weeks, integrating a nutritional label API with MongoDB and RESTful APIs for real-time calorie data and secure user authorization. The platform offers intuitive food tracking and nutritional insights, enhancing user engagement with health-focused features.</p>
+          <div className="text-right">
+            <a href="https://github.com/Le0uss/Bite_Balance" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
+              <FaGithub /> Check out my Project
+            </a>
           </div>
         </div>
-      ),
-    },
-    {
-      id: 2,
-      src: img2,
-      description: (
-        <div className="flex h-full">
-          <img src={img2} alt="Seat Booking App" className="w-1/2 h-auto rounded-lg" />
-          <div className="flex-grow flex flex-col justify-between ml-4">
-            <p>React and Node.js seat booking app. Integrates UI and server backend, focusing on error handling and server operations.</p>
-            <div className="text-right">
-              <a href="https://github.com/Le0uss" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
-                <FaGithub /> Check out my Project
-              </a>
-            </div>
+      </div>
+    ),
+  },
+  {
+    id: 2,
+    src: img2,
+    description: (
+      <div className="flex h-full">
+        <img src={img2} alt="Seat Booking App" className="w-3/5 h-auto rounded-lg" />
+        <div className="flex-grow flex flex-col justify-between ml-4">
+          <p>React and Node.js seat booking app. Integrates UI and server backend, focusing on error handling and server operations.</p>
+          <div className="text-right">
+            <a href="https://github.com/Le0uss/mongodb-P2" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
+              <FaGithub /> Check out my Project
+            </a>
           </div>
         </div>
-      ),
-    },
-    {
-      id: 3,
-      src: img3,
-      description: (
-        <div className="flex h-full">
-          <img src={img3} alt="Nyan Cat" className="w-1/2 h-auto rounded-lg" />
-          <div className="flex-grow flex flex-col justify-between ml-4">
-            <p>JavaScript OOP game for understanding OOP concepts. Players navigate as cheeseburgers avoiding Nyan Cats, focusing on debugging and customization.</p>
-            <div className="text-right">
-              <a href="https://github.com/Le0uss" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
-                <FaGithub /> Check out my Project
-              </a>
-            </div>
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    src: img3,
+    description: (
+      <div className="flex h-full">
+        <img src={img3} alt="Nyan Cat" className="w-1/2 h-auto rounded-lg" />
+        <div className="flex-grow flex flex-col justify-between ml-4">
+          <p>JavaScript OOP game for understanding OOP concepts. Players navigate as cheeseburgers avoiding Nyan Cats, focusing on debugging and customization.</p>
+          <div className="text-right">
+            <a href="https://github.com/Le0uss/project-js-nyan-cat" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
+              <FaGithub /> Check out my Project
+            </a>
           </div>
         </div>
-      ),
-    },
-    {
-      id: 4,
-      src: img4,
-      description: (
-        <div className="flex h-full">
-          <img src={img4} alt="Corner Cafe API Project" className="w-1/2 h-auto rounded-lg" />
-          <div className="flex-grow flex flex-col justify-between ml-4">
-            <p>RESTful API design for a cafe, covering endpoint creation and data management. Features GET, POST, DELETE operations and UUID use.</p>
-            <div className="text-right">
-              <a href="https://github.com/Le0uss" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
-                <FaGithub /> Check out my Project
-              </a>
-            </div>
+      </div>
+    ),
+  },
+  {
+    id: 4,
+    src: img4,
+    description: (
+      <div className="flex h-full">
+        <img src={img4} alt="E-Commerce" className="w-1/2 h-auto rounded-lg" />
+        <div className="flex-grow flex flex-col justify-between ml-4">
+          <p>Led a one-week intensive project to develop a dynamic e-commerce website using React, Node.js, Express, and MongoDB, incorporating RESTful APIs for a seamless user experience. 
+            Focused on building a responsive interface with efficient product browsing and secure payment integration.</p>
+          <div className="text-right">
+            <a href="https://github.com/RenaudCormierBootcamp/project-GROUP-e-commerce" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
+              <FaGithub /> Check out my Project
+            </a>
           </div>
         </div>
-      ),
-    },
-    {
-      id: 5,
-      src: img5,
-      description: (
-        <div className="flex h-full">
-          <img src={img5} alt="Slingshot Airlines Seat-Booking App" className="w-1/2 h-auto rounded-lg" />
-          <div className="flex-grow flex flex-col justify-between ml-4">
-            <p>Full-stack seat-booking system with frontend-backend integration. Features flight selection, reservation management, and MongoDB database integration.</p>
-            <div className="text-right">
-              <a href="https://github.com/Le0uss" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
-                <FaGithub /> Check out my Project
-              </a>
-            </div>
+      </div>
+    ),
+  },
+  {
+    id: 5,
+    src: img5,
+    description: (
+      <div className="flex h-full">
+        <img src={img5} alt="Slingshot Airlines Seat-Booking App" className="w-1/2 h-auto rounded-lg" />
+        <div className="flex-grow flex flex-col justify-between ml-4">
+          <p>Full-stack seat-booking system with frontend-backend integration. Features flight selection, reservation management, and MongoDB database integration.</p>
+          <div className="text-right">
+            <a href="https://github.com/lauschra/project-slingair" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
+              <FaGithub /> Check out my Project
+            </a>
           </div>
         </div>
-      ),
-    },
-    {
-        id: 6,
-        src: img6,
-        description: (
-          <div className="flex h-full">
-            <img src={img6} alt="Idle Game Project" className="w-1/2 h-auto rounded-lg" />
-            <div className="flex-grow flex flex-col justify-between ml-4">
-              <p>Idle game clone of Cookie Clicker. Focuses on simple but addictive gameplay mechanics, including resource generation and upgrades.</p>
-              <div className="text-right">
-                <a href="https://github.com/Le0uss" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
-                  <FaGithub /> Check out my Project
-                </a>
-              </div>
-            </div>
+      </div>
+    ),
+  },
+  {
+    id: 6,
+    src: img6,
+    description: (
+      <div className="flex h-full">
+        <img src={img6} alt="Idle Game Project" className="w-1/2 h-auto rounded-lg" />
+        <div className="flex-grow flex flex-col justify-between ml-4">
+          <p>Idle game clone of Cookie Clicker. Focuses on simple but addictive gameplay mechanics, including resource generation and upgrades.</p>
+          <div className="text-right">
+            <a href="https://github.com/Le0uss/react-effects" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 justify-end">
+              <FaGithub /> Check out my Project
+            </a>
           </div>
-        ),
-      }
-      
-  ];
+        </div>
+      </div>
+    ),
+  }
+];
+
   
 
 const generateSquares = (scrollToDescription) => {
